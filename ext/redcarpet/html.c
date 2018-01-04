f18bc350207f64df3e45a98d8db5fb1c8f492fb6
ishepherdMiner <iJason92@yahoo.com>
2018-01-04 11:38:49 +0800

    table 添加一个css类

--- ext/redcarpet/html.c
+++ ext/redcarpet/html.c
@@ -546,7 +546,7 @@ static void
 rndr_table(struct buf *ob, const struct buf *header, const struct buf *body, void *opaque)
 {
 	if (ob->size) bufputc(ob, '\n');
-	BUFPUTSL(ob, "<table><thead>\n");
+	BUFPUTSL(ob, "<table class=\"table\"><thead>\n");
 	if (header)
 		bufput(ob, header->data, header->size);
 	BUFPUTSL(ob, "</thead><tbody>\n");
